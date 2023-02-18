86aebcf82ba3cf4cd26a150e53ed37b19317a723
MartinsKing <42362589+ClydeTime@users.noreply.github.com>
2023-02-18 23:09:28 +0800

    Update noFocusAd.js

--- Script/XMLY/noFocusAd.js
+++ Script/XMLY/noFocusAd.js
@@ -1,6 +1,4 @@
-let body = JSON.parse($response.body);
-
-body.replace(/(\{"isAd.*?\})/g,x=>{
-   return x.match('isAd":true') ? '' : x ;
-})
-$done({ body: JSON.stringify(body) });
+let obj = JSON.parse($response.body)
+obj.header[0].item.list[0].data =
+obj.header[0].item.list[0].data.filter(x=>!x.isAd)
+$done({body:JSON.stringify(obj)})
