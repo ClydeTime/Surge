94f2c8faab2f0ce8666ae332587dc9af1561dc71
ClydeTime <clydetime@163.com>
2024-05-03 17:05:16 +0800

    update demo

--- Script/XMLY/xmly.js
+++ Script/XMLY/xmly.js
@@ -108,6 +108,82 @@ switch (path) {
 		delete body.newPage;
 		delete body.priceTypeId;
 		break;
+	case "/business-vip-level-h5-web/api/gift/detail/": //会员页
+		body.data.enable = true;
+		body.data.content.title = "V5会员等级_月度礼包";
+		body.data.content.id = 3003;
+		body.data.content.giftList = [
+			{
+					"id": "17401",
+					"description": "免费送知识红包\\n可送亲友6个",
+					"giftType": "28",
+					"context":
+					{
+							"giftCardId": "32",
+							"quantity": 6
+					},
+					"iconUrl": "",
+					"buttonContext": "去赠送",
+					"jumpUrl": "https://pages.ximalaya.com/mkt/act/ab1542ad75015a78",
+					"sortIndex": 1,
+					"mainTitle": "免费送知识红包",
+					"subTitle": "可送亲友6个"
+			},
+			{
+					"id": "11906",
+					"description": "免费送7天会员\\n可送亲友5张",
+					"giftType": "15",
+					"context":
+					{
+							"itemId": "1010500000114051",
+							"quantity": 5
+					},
+					"iconUrl": "",
+					"buttonContext": "去赠送",
+					"jumpUrl": "https://m.ximalaya.com/gatekeeper/universal-gift-card/gift-record?_default_share=0&giftType=1",
+					"sortIndex": 2,
+					"mainTitle": "免费送7天会员",
+					"subTitle": "可送亲友5张"
+			},
+			{
+					"id": "23467",
+					"description": "喜播课程大礼包\\n领5重好礼",
+					"giftType": "16",
+					"context":
+					{
+							"itemId": 1227,
+							"itemKey": "2f14f8df60ad69e2"
+					},
+					"iconUrl": "",
+					"buttonContext": "去使用",
+					"jumpUrl": "https://m.ximalaya.com/gatekeeper/lms-sale-page?expGroupId=33&xbSource=55395",
+					"sortIndex": 3,
+					"mainTitle": "喜播课程大礼包",
+					"subTitle": "领5重好礼"
+			},
+			{
+					"id": "13656",
+					"description": "直播贵族体验卡\\n伯爵7天",
+					"giftType": "21",
+					"context":
+					{
+							"itemId": 3085
+					},
+					"iconUrl": "https://imagev2.xmcdn.com/storages/29e2-audiofreehighqps/78/6C/GKwRIMAGo_-AAAArZQF9iI1h.png",
+					"buttonContext": "去使用",
+					"jumpUrl": "iting://open?msg_type=330&type=1&playSource=4055",
+					"sortIndex": 4,
+					"mainTitle": "直播贵族体验卡",
+					"subTitle": "伯爵7天"
+			}
+		];
+		break;
+	case "/business-vip-level-h5-web/api/profile": //会员页
+		body.data.vipProfileVo.expire = "2029-12-31 23:59:59";
+		body.data.vipProfileVo.level = 5;
+		body.data.vipProfileVo.value = 28888;
+		body.data.vipProfileVo.state = 4;
+		break;
 	case "/business-vip-presale-mobile-web/page": //我的会员页
 		body.data.modules = body.data.modules.map(module => {
 			if(module.key === "userInfo"){
@@ -143,12 +219,24 @@ switch (path) {
 			return module;
 		});
 		break;
+	case "/business-vip-welfare-mobile-web/welfare/module/exclusive/list": //会员权益广告
+		body.data.level = 5;
+		body.data.templateVos = [];
+		break;
 	case "/browsing-history-business/browsing/history/query": //我的--历史--浏览历史
 		body.data.list = body.data.list.map(list => { //list.type 1为有声书 2为直播（考虑去除）
 			delete list.albumSubscript;
 			list.paidAlbum = false;
 			return list;
 		});
+		break;
+	case "/discovery-category/customCategories": //首页导航栏
+		const ids = ['lamia', 'html5-194', 'html5-97', 'single_category-1054'];
+		body.categoryList && (body.categoryList = body.categoryList.map(categoryList => {
+			return categoryList.itemList.filter(itemList => !ids.includes(itemList.id));
+		}))
+		body.customCategoryList && (body.customCategoryList = body.customCategoryList.filter(customCategoryList => !ids.includes(customCategoryList.id)))
+		body.defaultTabList && (body.defaultTabList = body.defaultTabList.filter(defaultTabList => !ids.includes(defaultTabList.id)))
 		break;
 	case "/discovery-feed/v1/freeListenTab/queryCardList": //底部免费页
 		body.sceneCards = body.sceneCards.map(sceneCards => {
@@ -186,13 +274,12 @@ switch (path) {
 		body.body = body.body.filter(body => body.mtlId !== 0);
 		body.header && (body.header = body.header.filter(header => header.item.moduleType === "square"));
 		break;
-	case "/discovery-category/customCategories": //首页导航栏
-		const ids = ['lamia', 'html5-194', 'html5-97', 'single_category-1054'];
-		body.categoryList && (body.categoryList = body.categoryList.map(categoryList => {
-			return categoryList.itemList.filter(itemList => !ids.includes(itemList.id));
-		}))
-		body.customCategoryList && (body.customCategoryList = body.customCategoryList.filter(customCategoryList => !ids.includes(customCategoryList.id)))
-		body.defaultTabList && (body.defaultTabList = body.defaultTabList.filter(defaultTabList => !ids.includes(defaultTabList.id)))
+	case "/discovery-ranking-web/v6/ranking/concreteRankList": //排行榜
+		body.data.subElements = body.data.subElements.map(subElement => {
+			subElement.status.isPaid = 0;
+			subElement.wrap.subscript = null;
+			return subElement;
+		});
 		break;
 	case "/focus-mobile/focusPic/info": //首页广告轮盘
 		body.header = body.header.filter(header => {
