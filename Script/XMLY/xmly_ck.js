6a49172732f967cbd582fad86e2c1e15e6d5324e
ClydeTime <clydetime@163.com>
2024-05-16 17:25:47 +0800

    更新解锁xmly会员

---
+++ Script/XMLY/xmly_ck.js
@@ -0,0 +1,4 @@
+let headers = $request["headers"];
+headers["cookie"] =
+  "channel=ios-b1;  device_model=iPhone%2012%20Pro; XD=MLWBt9QyvUucifxUg0TSATBTe4NbpfPKlagjhAJSLndXM3FhGGq6yxOB20Xm81uDxQJn8Gc0OuYXDWIhwM6KAQ==; fp=0092v7647x2022b2236490501b0000a120212200100000001101411004030; pcdnFree=1";
+$done({ 'headers': headers });
